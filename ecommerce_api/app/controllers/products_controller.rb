2b9da45f102c0c639abd7c0f91621b8ccceb26dc
Lucas Borges <lucas-borges2000@hotmail.com>
2025-01-06 19:00:37 -0300

    Subindo primeira parte do projeto

---
+++ ecommerce_api/app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  # GET /products
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  # GET /products/:id
+  def show
+    render json: @product
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT/PATCH /products/:id
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/:id
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock)
+  end
+end
